1eeeb7e8d7c3a354bab94c84b60631dcaffa2e93
Nova Zackrisson <novazackrisson@airsomtlhornova.home>
2024-08-29 10:49:35 +0200

    All exercises done

--- src/index.js
+++ src/index.js
@@ -19,24 +19,43 @@ const pokemons = [
 // the pokemons from the pokemons array
 // invoke the function to see that it works
 
+pokemons.forEach((name => {
+  console.log(name)
+}))
+
 // 2)
 // Create an object for the pokemon Pikachu.
 // it could have properties such as name, element,
 // strength, color, amountOfLegs or something like that.
+const pokemonP = {
+  name: 'Pikachu',
+  strength: 28,
+  element: 'electric',
+  color: 'yellow',
+  amountOfLegs: 2
+}
 
 // 3)
 // console.log something like
 // 'Pikachu is a electricity pokemon with the strength of 32'
 // Selecting some of the information about your pikachu object.
 
+console.log(`${pokemonP.name} is an ${pokemonP.color} ${pokemonP.element} pokemon with the strenght of`, pokemonP.strength)
+
 // 4)
 // Add the property stillToCatch: true to the Pikachu object.
+
+pokemonP.stillToCatch = true
 
 // 5)
 // change the value of the key strength in the Pikachu object.
 
+pokemonP.strength = 55
+
 // 6)
 // Delete a property from the Pikachu object.
+
+delete pokemonP.amountOfLegs
 
 // 7)
 // Fill up this array with a few more pokemons.
@@ -53,13 +72,36 @@ const moreAboutPokemons = [
     name: "Charmander",
     element: "fire",
     strength: 32
+  },
+  {
+    name: "Ivysaur",
+    element: "ice",
+    strength: 40
+  },
+  {
+    name: "Weedle",
+    element: "wind",
+    strength: 51
+  },
+  {
+    name: "Squirtle",
+    element: "dirt",
+    strength: 18
   }
 ];
 
 // 8)
 // Print out all the NAMES of the pokemons in this new array.
 
+moreAboutPokemons.forEach(pokemon => {
+  console.log(pokemon.name);
+});
+
 // 9)
 // Print out something like
 // 'Bulbasaur is a water pokemon with the strength of 32'
 // Do this for all the pokemons.
+
+moreAboutPokemons.forEach(pokemon => {
+  console.log(`${pokemon.name} is a ${pokemon.element} pokemon with the strenght of`, pokemon.strength);
+});
